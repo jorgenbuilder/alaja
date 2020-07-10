f8b2d11518ecd345c19d65f8066919fbd74c68bb
Jorgen <jorgen@hookham>
2020-07-10 10:53:51 -0700

    use s3 for active storage

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,12 @@ module AlajaWeb
     # the framework and any gems in your application.
 
     config.web_console.whitelisted_ips = '172.22.0.1'
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = ENV.fetch("AWS_S3_BUCKET_NAME") ? :aws : :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
