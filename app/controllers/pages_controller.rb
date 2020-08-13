28bd8ca97d133e024969b9325057831b523a03ac
jorgen <jorgen@buildnode.io>
2020-08-12 17:42:49 -0700

    reverse order of sessions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
     def home
         @band_name = BandName.last
         logger.debug "Someone named the band #{@band_name.name} on #{@band_name.date}"
-        @sessions = Session.all
+        @sessions = Session.all.order(date: :desc)
     end
 end
